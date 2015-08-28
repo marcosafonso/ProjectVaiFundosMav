2e7d760934923cd79f4d6216498d505f38c3c11d
marcosafonso <afonso-vieira@hotmail.com>
2015-08-28 18:24:43 -0300

    teste

---
+++ VaiFundos/VaiFundos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaiFundos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello VGit Hub :), vamos Programar!");
+            Console.ReadKey();
+        }
+    }
+}
