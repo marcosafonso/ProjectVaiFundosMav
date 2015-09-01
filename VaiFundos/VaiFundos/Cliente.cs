18e5aeaf90326875a9e4476ca3acf8317e3dfc95
marcosafonso <afonso-vieira@hotmail.com>
2015-09-01 09:38:38 -0300

    classe cliente

---
+++ VaiFundos/VaiFundos/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaiFundos
+{
+    class Cliente
+    {
+        private int codigo;
+        private string nome;
+
+        public int getCodigo()
+        {
+            return codigo;
+        }
+
+        public void setCodigo(int codigo)
+        {
+            this.codigo = codigo;
+        }
+        //
+        public string getNome()
+        {
+            return nome;
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+    }
+}
