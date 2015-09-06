8fd01679db049ac0485dd89bf33429d8d54ae143
elielsonbs <elielsonbs@hotmail.com>
2015-09-06 15:16:02 -0300

    Criação da Classe Aplicação

---
+++ VaiFundos/VaiFundos/Aplicacao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VaiFundos
+{
+    class Aplicacao
+    {
+        private int codCliente;
+        private string nomeAplicacao;
+        private double valor;
+
+        public int getCodCliente()
+        {
+            return codCliente;
+        }
+
+        public string getNomeApli()
+        {
+            return nomeAplicacao;
+        }
+
+        public void setValor(double valor)
+        {
+            this.valor = valor;
+        }
+
+        public double getValor()
+        {
+            return valor;
+        } 
+    }
+}
